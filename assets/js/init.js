0ec3b767f30fa1b9271595df5805913775484967
JD Flynn <jflynn8@gmail.com>
2018-09-23 19:04:25 -0500

    initial commit

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,10 +1,36 @@
  (function($){
    $(function(){
-
+    var slideTimeout;
      $('.button-collapse').sideNav();
      $('.parallax').parallax();
      $('.collapsible').collapsible();
-     $('.carousel.carousel-slider').carousel({fullWidth: true});
+     $('.carousel.carousel-slider').carousel({
+       fullWidth: true
+      });
+     setTimeout(autoplay, 7000);
+     function autoplay() {
+       $('.carousel.carousel-slider').carousel('next');
+       slideTimeout = setTimeout(autoplay, 7000);
+     }
+     // move next carousel
+     $('.moveNextCarousel').click(function (e) {
+       e.preventDefault();
+       e.stopPropagation();
+       $('.carousel').carousel('next');
+     });
+
+     // move prev carousel
+     $('.movePrevCarousel').click(function (e) {
+       e.preventDefault();
+       e.stopPropagation();
+       $('.carousel').carousel('prev');
+     });
+
+     $('.pauseCarousel').click(function (e) {
+       e.preventDefault();
+       e.stopPropagation();
+       clearTimeout(slideTimeout);
+     })
      $('.materialboxed').materialbox();
      $('.scrollspy').scrollSpy();
      $('.tap-target').tapTarget('open');
